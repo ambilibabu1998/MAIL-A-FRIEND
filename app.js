7c2d7c1972e52a67af0231a5421557fa7b541769
ambilibabu1998 <121178223+ambilibabu1998@users.noreply.github.com>
2023-03-24 12:31:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: 'ambilibabu1998@gmail.com', // your email address
+        pass: 'itvzccjisdsjwuta' // your email password
+    }
+});
+
+// setup email data with unicode symbols
+let mailOptions = {
+    from: '"AMBILI BABU" <ambilibabu1998@gmail.com>', // sender address
+    to: 'ambili.babu.sheeba@gmail.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello Ammu? Hope you are doing well', // plain text body
+    html: '<b>Hello Ammu?Hope you are doing well</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+});
+
